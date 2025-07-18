c95450a5a1da2f4b49e3d0f9f294defbf0d0513d
Neruthes <i@neruthes.xyz>
2025-07-18 00:55:30 +0000

    website: add yearslist and year

--- website/build.sh
+++ website/build.sh
@@ -12,7 +12,7 @@
 (cd src && bash build.sh)
 
 
-issues_arr="$(find ../issue -name dist.json | cut -d/ -f4 | cut -c1-6 | grep -v 000000 | sort -u)"
+issues_arr="$(find ../issue -name dist.json | cut -d/ -f4 | cut -c1-6 | grep -v 000000 | sort -ur)"
 
 
 ### Step: Fetch issues data
@@ -26,7 +26,7 @@ done <<< "$issues_arr"
 
 
 
-### Step: All articles
+# ### Step: All articles
 while read -r IssueID; do
     year="${IssueID:0:4}"
     issue_dir="../issue/$year/$IssueID.tex.d"
@@ -44,11 +44,42 @@ done <<< "$issues_arr"
 mustache <(tomlq -r . site.toml) src/templates/home.html > "build/index.html"
 
 
+
+### Step: Years
+mkdir -p data/year
+printf '' > data/years.txt
+find ../issue/ -maxdepth 1 -mindepth 1 -type d | cut -d/ -f3 | sort -r | grep -v 0000 | while read -r year; do
+    find "../issue/$year" -name dist.toml &&
+    echo "$year" >> data/years.txt
+done
+(
+    echo '[[year]]'
+    while read -r year; do
+        printf -- 'year_id = "%s"\n' "$year"
+    done < data/years.txt
+) | tomlq -r . > data/years.json
+cat data/years.json
+mustache data/years.json src/templates/yearslist.html > "build/issue/index.html"
+
+
+
+### Step: Each year
+while read -r year; do
+    (
+        printf -- 'year = "%s"\n' "$year"
+        grep "^$year" <<< "$issues_arr" | sort -r | while read -r IssueID; do
+            echo "[[issue]]"
+            printf 'issue_id = "%s"\n' "$IssueID"
+        done
+    ) | tomlq -r . > "data/year/$year.json"
+    mustache "data/year/$year.json" src/templates/year.html > "build/issue/$year/index.html"
+done < data/years.txt
+
+
+
 mkdir -p static/
 rsync -auvpx --mkpath static/ www/static/
 rsync -auvpx --mkpath ../vi/ www/static/vi/
 rsync -auvpx --mkpath build/ www/
 rsync -auvpx --mkpath data/ www/data/
 
-# find data -type f -delete
-# find build -type f -delete
