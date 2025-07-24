830754db07f5f98520f174a32aaf4fb5dbe62bc9
Neruthes <i@neruthes.xyz>
2025-07-24 17:33:50 +0000

    website: fix

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@
 
 
 wrangler pages deploy website/www --project-name="weightintheattention" --commit-dirty=true --branch=main && printf "Upload finished\n\n"
+
+if [[ -d ../wita-journal.github.io ]]; then
+    rsync --dry-run -auvpx --mkpath website/www/ ../wita-journal.github.io/docs/
+fi
